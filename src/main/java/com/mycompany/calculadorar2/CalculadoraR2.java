209d644c5ba39446988f083e9657b99358b14a38
M4kyy <gonzalocuk735@gmail.com>
2023-08-18 17:01:19 -0300

    Initial commit

---
+++ src/main/java/com/mycompany/calculadorar2/CalculadoraR2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculadorar2;
+
+/**
+ *
+ * @author SCP-062
+ */
+public class CalculadoraR2 {
+
+    public static void main(String[] args) {
+        
+    }
+}
